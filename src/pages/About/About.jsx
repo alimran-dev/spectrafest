db7b8f3959840fb90258557f669fd9baffa343b7
alimran-dev <alimran5air@gmail.com>
2023-10-09 04:25:23 +0600

    about page added

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,116 @@
 const About = () => {
-    return (
-        <div>
-            Hello from about
-        </div>
-    );
+  return (
+    <div className="px-20 py-10 space-y-5">
+      <h1 className="w-fit mx-auto text-transparent text-4xl font-bold bg-gradient-to-r from-[#EB3B2E] to-[#BE6524] bg-clip-text">
+        &quot;SpectraFest Event Solutions&quot;
+      </h1>
+      <p className="text-center font-medium">
+        Welcome to SpectraFest Event Solution, where we turn your entertainment
+        dreams into reality! We are your dedicated partner in creating
+        unforgettable and seamless entertainment experiences. Our mission is to
+        elevate the art of event management, setting new standards of excellence
+        in the industry.
+      </p>
+      <div>
+        <h2 className="w-fit text-transparent text-2xl font-bold bg-gradient-to-r from-[#EB3B2E] to-[#BE6524] bg-clip-text mb-1">
+          Our Story
+        </h2>
+        <p className="font-medium">
+          SpectraFest Event Solution was born out of a shared passion for
+          entertainment and a deep understanding of what it takes to bring
+          exceptional events to life. Established by a team of seasoned
+          professionals with years of experience in event planning, production,
+          and management, our company is built on a foundation of creativity,
+          innovation, and a commitment to delivering unparalleled results.
+        </p>
+      </div>
+      <div>
+        <h2 className="w-fit text-transparent text-2xl font-bold bg-gradient-to-r from-[#EB3B2E] to-[#BE6524] bg-clip-text mb-1">
+          What We Do
+        </h2>
+        <p className="font-medium">
+          At SpectraFest Event Solution, we specialize in crafting and managing
+          a wide range of entertainment events. From music festivals and
+          concerts to corporate gatherings, weddings, and community
+          celebrations, our expertise knows no bounds. We take pride in our
+          ability to transform your vision into a reality, no matter how big or
+          small the event.
+        </p>
+      </div>
+      <div>
+        <h2 className="w-fit text-transparent text-2xl font-bold bg-gradient-to-r from-[#EB3B2E] to-[#BE6524] bg-clip-text mb-1">
+          Our Approach
+        </h2>
+        <p className="font-medium">
+          We believe that every event is unique and should be treated as such.
+          That&apos;s why we take a personalized approach to every project,
+          working closely with our clients to understand their goals,
+          preferences, and expectations. Our team of skilled professionals
+          combines creative ingenuity with meticulous planning to design events
+          that leave a lasting impression.
+        </p>
+      </div>
+      <div>
+        <h2 className="w-fit text-transparent text-2xl font-bold bg-gradient-to-r from-[#EB3B2E] to-[#BE6524] bg-clip-text mb-1">
+          Why Choose Us
+        </h2>
+        <p className="font-medium">
+          <p>
+            <span className="font-bold">Experience:</span> With years of
+            industry experience, we bring a wealth of knowledge and expertise to
+            every project.
+          </p>
+          <p>
+            <span className="font-bold">Creativity:</span> We thrive on
+            innovation and continuously explore new trends and ideas to keep
+            your events fresh and exciting.
+          </p>
+          <p>
+            <span className="font-bold">Dedication:</span> Your satisfaction is
+            our top priority. We are committed to going above and beyond to
+            exceed your expectations.
+          </p>
+          <p>
+            <span className="font-bold">Reliability:</span> You can count on us
+            to deliver on time and within budget, ensuring a stress-free event
+            planning process.
+          </p>
+          <p>
+            <span className="font-bold">Network:</span> We have a vast network
+            of trusted vendors, artists, and partners to ensure that every
+            aspect of your event is top-notch.
+          </p>
+        </p>
+      </div>
+      <div>
+        <h2 className="w-fit text-transparent text-2xl font-bold bg-gradient-to-r from-[#EB3B2E] to-[#BE6524] bg-clip-text mb-1">
+          Our Team
+        </h2>
+        <p className="font-medium">
+          Our team consists of talented event planners, designers, production
+          experts, and project managers who are passionate about what they do.
+          Together, we work tirelessly to make every event a resounding success.
+        </p>
+      </div>
+      <div>
+        <h2 className="w-fit text-transparent text-2xl font-bold bg-gradient-to-r from-[#EB3B2E] to-[#BE6524] bg-clip-text mb-1">
+          Get in Touch
+        </h2>
+        <p className="font-medium">
+          We are excited to embark on this entertainment journey with you. If
+          you&apos;re ready to create an event that will leave a lasting
+          impression, don&apos;t hesitate to get in touch with us. We look
+          forward to hearing about your ideas and turning them into spectacular
+          realities.
+        </p>
+      </div>
+      <p className="text-center font-medium w-1/2 mx-auto">
+        Thank you for considering SpectraFest Event Solution as your partner in
+        entertainment event management. Let&apos;s create memorable moments
+        together!
+      </p>
+    </div>
+  );
 };
 
 export default About;
